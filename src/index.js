5bb4a68dbc9e617c5ba4500094332d29e2f8d382
akhfas <zakaria@aksaramaya.com>
2021-04-19 22:55:02 +0700

    add

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dopassss.us.auth0.com"
+    clientId="1GR6qYwtHdyjFLikqlNq6ffkr060aBjl"
+    redirectUri="http://localhost:3000"
+    scope="read:current_user update:current_user_metadata"
+  >
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Auth0Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
